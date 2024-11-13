87eb12dc610dcb1f90c2e8eaa93bb65a4da235bd
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-13 19:35:52 +0000

    Created list.php

---
+++ src/views/list.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bank Account</title>
+</head>
+<body>
+  <h1>List</h1>
+  <table>
+    <thead>
+      <tr>
+        <th>Account Number</th>
+        <th>Name</th>
+        <th>Balance </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </tbody>
+  </table>
+</body>
+</html>
