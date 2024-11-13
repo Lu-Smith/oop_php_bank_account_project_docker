9ce8b9f76f765cadfeddd9e44fbfc2d0ee036344
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-13 20:07:46 +0000

    Updated index.php

--- src/public/index.php
+++ src/public/index.php
@@ -2,5 +2,9 @@
 
 declare(strict_types=1);
 
+require_once '../helpers/loadAccounts.php';
 require_once './bankAccount.php';
+
+$accounts = loadAccountsFromCSV(__DIR__ . '/../accounts.csv');
+
 require_once '../views/list.php';
