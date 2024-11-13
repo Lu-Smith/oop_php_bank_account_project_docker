58917e77d1ca140fc01783a4b19facf65969ee8d
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-11-13 22:23:13 +0000

    Updated list.php

--- src/views/list.php
+++ src/views/list.php
@@ -12,7 +12,8 @@
       <tr>
         <th>Account Number</th>
         <th>Name</th>
-        <th>Balance </th>
+        <th>Balance (£)</th>
+        <th>Change</th>
       </tr>
     </thead>
     <tbody>
@@ -21,6 +22,14 @@
         <td><?= htmlspecialchars($account->getAccountNumber()) ?></td>
         <td><?= htmlspecialchars($account->getName()) ?></td>
         <td><?= htmlspecialchars(number_format($account->getBalance(), 2)) ?></td>
+        <td>
+          <form action="process_transaction.php" method="POST">
+            <input type="number" name="deposit_amount" placeholder="deposit">
+            <button type="submit" name="action" value="deposit">Submit Deposit</button>
+            <input type="number" name="withdraw_amount" placeholder="withdraw">
+            <button type="submit" name="action" value="withdraw">Submit Withdraw</button>
+          </form>
+        </td>
       </tr>
       <?php endforeach ?>
     </tbody>
